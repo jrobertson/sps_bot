b365542b9c4216bd37052d36ab791101eed03517
Your Name <you@example.com>
2016-07-27 21:49:42 +0100

    feature: sps_bot now uses the botbase gem for its functionality.

--- lib/sps_bot.rb
+++ lib/sps_bot.rb
@@ -3,50 +3,29 @@
 # file: sps_bot.rb
 
 require 'sps_chat'
+require 'botbase'
 
 
 class SpsBot < SPSChat  
   
-  def initialize(host: 'localhost', port: '8080', userid: 'bot', 
-                 service_modules: nil, room: '')
+  def initialize(host: '127.0.0.1', port: '8080', userid: 'bot', room: '', 
+                 config: nil, typing_mode: false)
     
     super(host: host, port: port, userid: userid, room: room)
-   
-    @phrases = {}
-    
-    a = if service_modules then
-    
-      service_modules
-
-    else
-      
-      h = {
-          "^What's the time?" => "'The time is ' + Time.now.strftime(\"%-I:%M%p\")",
-          "hello?" => %q("hello #{sender}, how can I help you?" % sender),
-          'unknown' => "\"I'm sorry, I don't understand what you said.\""
-      }
-      [h]
-
-    end
-    
-    a.each {|x|  @phrases.merge! x }
+    @bot = BotBase.new config
+    @typing_mode = typing_mode
     
   end  
 
-  def onincoming(sender, msg)
+  def onincoming(sender, msg, typing_mode=false)
     
-    r = @phrases.detect {|k,v|  msg =~ /#{k}/i }
-
-    raw_msg = if r then
-      r.last 
-    else  
-      unknown = h['unknown']
-      unknown.last if unknown
+    if not typing_mode then
+      #msg = 'tim'
+      
+      response = @bot.received sender, msg
+    
+      self.send response
     end
-    
-    response = eval raw_msg
-    
-    self.send response
 
   end
 
